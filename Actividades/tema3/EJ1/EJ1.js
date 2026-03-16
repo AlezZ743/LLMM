f22022bc37f443da77c0ef2600a11eb74aea425c
AlezZ743 <144882308+AlezZ743@users.noreply.github.com>
2026-03-16 13:14:03 +0100

    ejs

---
+++ Actividades/tema3/EJ1/EJ1.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+
+function clearText() {
+    document.getElementById("demo").innerHTML = "";
+}
---
+++ Actividades/tema3/EJ2/EJ2.js
@@ -0,0 +1,4 @@
+// This function finds the 'demo' element and updates its text
+function writeMessage() {
+    document.getElementById("demo").innerHTML = "Hello world!";
+}
